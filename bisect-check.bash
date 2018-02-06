e3c5111ce54e400198404f58f48e2df6668aaa7c
Zoltan Haindrich <kirk@rxd.hu>
2018-02-06 10:04:48 +0100

    update script

--- bisect-check.bash
+++ bisect-check.bash
@@ -1,18 +1,29 @@
 #!/bin/bash
-
 set -e
+#set -x
 
-HASH=09ce6cdf3fd3682c2501ca0b5664cfc2adf6b765
-M_OPTS="-pl beeline -Dtest=TestHiveCli -q"
+C=09ce6cdf3fd3682c2501ca0b5664cfc2adf6b765
 
-git clone github.com/apache/hive
-cd hive
-echo "@@@ BEFORE"
-git checkout ${HASH}^
-mvn install $M_OPTS
+M_OPTS="
+-Dorg.slf4j.simpleLogger.log.org.apache.maven.plugin.surefire.SurefirePlugin=INFO
+-q -Pitests
+-DskipSparkTests
+-Dtest=TestHiveCli
+-pl beeline -am
+install
+"
+export M_OPTS
 
+git log -n1 $C
+
+echo "@@@ check good"
 git clean -dfx
+git checkout ${C}^
+time mvn $M_OPTS
 
-echo "@@@ AFTER"
-git checkout ${HASH}
-mvn install $M_OPTS
+echo "@@@ check bad"
+git clean -dfx
+git checkout ${C}
+time mvn $M_OPTS
+
+echo "@@@ I was wrong..."
