5e7a22a9fbd252aeda96ec2020cbe8f19bfa7c8c
Zoltan Haindrich <kirk@rxd.hu>
2018-04-11 11:51:37 +0200

    update filter

--- ptest_peek.bash
+++ ptest_peek.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 QUEUE_FILE=/tmp/.asf_queue
-wget -q -O $QUEUE_FILE "https://builds.apache.org/queue/api/xml?tree=items[actions[parameters[name,value]],task[name]]"
+wget -q -O $QUEUE_FILE "https://builds.apache.org/queue/api/xml?tree=items[actions[causes[userId],parameters[name,value]],task[name],id,inQueueSince]"
 
 n=`xmlstarlet fo -t $QUEUE_FILE | grep PreCommit-HIVE-Build | wc -l`
 
