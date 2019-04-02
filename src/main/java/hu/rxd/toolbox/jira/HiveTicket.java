fb27d687fbcda5955ec656f9fbd0ce8e96a8ffa2
Zoltan Haindrich <kirk@rxd.hu>
2019-04-02 08:51:18 +0200

    ignore yetus messages

--- src/main/java/hu/rxd/toolbox/jira/HiveTicket.java
+++ src/main/java/hu/rxd/toolbox/jira/HiveTicket.java
@@ -101,7 +101,10 @@ public class HiveTicket {
 
   public Comment getLastQAComment() {
     List<Comment> comments = getCommentsByName(HIVEQA);
-    comments.removeIf((Comment c) -> {return c.getBody().contains("-1 due to build exiting with an error");} );
+    comments.removeIf((Comment c) -> {
+      String body = c.getBody();
+      return body.contains("-1 due to build exiting with an error") || body.contains("Apache Yetus");
+    });
     if (comments.size() == 0) {
       throw new RuntimeException("theres no last qa comment");
     }
