52810282f9f3f86a47b2a8d2a03527265e6ee6a7
Zoltan Haindrich <kirk@rxd.hu>
2019-09-30 11:40:54 +0200

    use alternate mirror

--- src/main/java/hu/rxd/toolbox/switcher/HdpMirrors.java
+++ src/main/java/hu/rxd/toolbox/switcher/HdpMirrors.java
@@ -11,13 +11,15 @@ public class HdpMirrors implements Mirrors {
   @Override
   public String getComponentVersion(Version version, Component c) throws Exception {
     String artifacts =
-        String.format("http://public-repo-1.hortonworks.com/HDP/centos7/3.x/updates/%s/artifacts.txt",
+        String.format("http://private-repo-1.hortonworks.com/HDP/centos7/3.x/updates/%s/artifacts.txt",
+            //              String.format("http://public-repo-1.hortonworks.com/HDP/centos7/3.x/updates/%s/artifacts.txt",
             version.stackVersion);
     return CDPMirrors.determineComponentVerFromArtifactsTxt(artifacts, version, c);
   }
 
   private static final List<String> MIRROR_ROOTS =
-      Lists.newArrayList(("http://public-repo-1.hortonworks.com/HDP"));
+      Lists.newArrayList("http://public-repo-1.hortonworks.com/HDP",
+          "http://private-repo-1.hortonworks.com/HDP");
 
   public static Collection<Mirror> of(Version ver) {
     List<Mirror> ret = new ArrayList<>();
